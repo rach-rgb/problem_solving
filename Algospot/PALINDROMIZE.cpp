c430334f9f41e74d3a5bc4b01c7f02b7a534d00e
seha park <72252232+rach-rgb@users.noreply.github.com>
2021-07-02 15:16:41 +0900

    Update PALINDROMIZE.cpp
    2021-07-02

--- Algospot/PALINDROMIZE.cpp
+++ Algospot/PALINDROMIZE.cpp
@@ -1,88 +1,70 @@
 #include <iostream>
-#include <string>
-#include <cstring>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-#define MAXS (100000)
+string N, M;
+vector<int> pmatch;
 
-void solve();
-void failure();
+void calculate() {
+	pmatch = vector<int>(M.length(), 0);
 
-string S;
-string SR; // reversed string
-int fail[MAXS]; // fail function for reversd string
-int result;
+	int begin = 1, matched = 0;
+	while (begin + matched < M.length()) {
+		if (M[begin + matched] == M[matched]) {
+			matched++;
+			pmatch[begin + matched - 1] = matched;
+		}
+		else {
+			if (matched == 0) {
+				begin++;
+			}
+			else {
+				begin = begin + matched - pmatch[matched - 1];
+				matched = pmatch[matched - 1];
+			}
+		}
+	}
+}
 
-int main()
-{
-	ios::sync_with_stdio(false);
+int solve() {
+	int result = 0;
+
+	calculate();
+
+	int begin = 0, matched = 0;
+	while (begin + matched < N.length()) {
+		if (N[begin + matched] == M[matched]) {
+			matched++;
+			if (begin + matched == N.length()) {
+				result = matched;
+				break;
+			}
+		}
+		else {
+			if (matched == 0) {
+				begin++;
+			}
+			else {
+				begin = begin + matched - pmatch[matched - 1];
+				matched = pmatch[matched - 1];
+			}
+		}
+	}
+
+	return 2 * N.size() - result;
+}
+
+
+int main() {
 	int T;
 	cin >> T;
-	for (int i = 0; i < T; i++)
-	{
-		cin >> S;
-		SR = S;
-		reverse(SR.begin(), SR.end());
-		solve();
-		cout << result << endl;
+	while (T--) {
+		cin >> N;
+		M = N; reverse(M.begin(), M.end());
+
+		cout << solve() << endl;
 	}
 
 	return 0;
-}
-
-// construct failure function
-void failure()
-{
-	memset(fail, 0, sizeof(int) * MAXS);
-
-	int begin = 1, matched = 0;
-	while (begin + matched < SR.size())
-	{
-		if (SR[begin + matched] == SR[matched])
-		{
-			matched++;
-			fail[begin + matched - 1] = matched;
-		}
-		else
-		{
-			if (matched == 0)
-			{
-				begin++;
-			}
-			else
-			{
-				begin = begin + matched - fail[matched - 1];
-				matched = fail[matched - 1];
-			}
-		}
-	}
-}
-
-void solve()
-{
-	result = 0;
-
-	int begin = 0, matched = 0;
-	while (begin < S.size())
-	{
-		if ((matched < S.size()) && (S[begin + matched] == SR[matched]))
-		{
-			matched++;
-			if (begin + matched == S.size()) { break; }
-		}
-		else
-		{
-			if (matched == 0)
-			{
-				begin++;
-			}
-			else
-			{
-				begin = begin + matched - fail[matched - 1];
-				matched = fail[matched - 1];
-			}
-		}
-	}
-	result = S.size() * 2 - matched;
 }
