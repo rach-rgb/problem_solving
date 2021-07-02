ac248689faa2c2e8df108e96e695f7b455a34069
seha park <72252232+rach-rgb@users.noreply.github.com>
2021-07-02 14:57:42 +0900

    Update NAMING.cpp
    2021-07-02

--- Algospot/NAMING.cpp
+++ Algospot/NAMING.cpp
@@ -1,65 +1,59 @@
 #include <iostream>
-#include <string>
-#include <cstring>
 #include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-#define MAXS 400000
+string N, M;
+vector<int> pmatch;
 
-void solve();
+void calculate() {
+	pmatch.resize(N.length());
 
-string s;
-int f[MAXS]; // f[x] = y: longest suffix for [0:x] is length y
-vector<int> result;
-
-int main(void)
-{
-	ios::sync_with_stdio(false);
-
-	string s1, s2;
-	cin >> s1 >> s2;
-	s = s1 + s2;
-	solve();
-	for (int i = 0; i < result.size(); i++)
-	{
-		cout << result[result.size() - i - 1] << " ";
-	}
-	cout << endl;
-	
-	return 0;
-}
-
-void solve()
-{
 	int begin = 1, matched = 0;
-	result.clear();
-
-	memset(f, 0, sizeof(int) * s.size());
-
-	// construct failure function
-	while (begin + matched < s.size())
-	{
-		if (s[begin + matched] == s[matched])
-		{
+	while (begin + matched < N.length()) {
+		if (N[begin + matched] == M[matched]) {
 			matched++;
-			f[begin + matched - 1] = matched;
+			pmatch[begin + matched - 1] = matched;
 		}
-		else
-		{
-			if (matched == 0)
-				++begin;
-			else {
-				begin = begin + matched - f[matched - 1];
-				matched = f[matched - 1];
+		else {
+			if (matched == 0) { begin++; }
+			else
+			{
+				begin = begin + matched - pmatch[matched - 1];
+				matched = pmatch[matched - 1];
 			}
 		}
 	}
+}
 
-	result.push_back(s.size());
-	int k = f[s.size() - 1];
-	while (k > 0)
-	{
-		result.push_back(k);
-		k = f[k - 1];
+vector<int> solve() {
+	vector<int> result;
+
+	calculate(); // precalculate partial match
+
+	int matched = N.length();
+	while (matched > 0) {
+		result.push_back(matched);
+		matched = pmatch[matched - 1];
 	}
+
+	reverse(result.begin(), result.end());
+
+	return result;
+}
+
+int main() {
+	string s1, s2;
+	cin >> s1 >> s2;
+	N = s1 + s2;
+	M = s1 + s2;
+
+	vector<int> result = solve();
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
 }
